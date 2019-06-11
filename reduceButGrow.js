ec599e6d27bb4360d2191b5004363d47c9296aa6
Jonathan Keane <jkeane889@gmail.com>
2019-06-11 13:34:20 -0400

    Short ES5 function to calculate multiplication sum of array elements

---
+++ reduceButGrow.js
@@ -0,0 +1,24 @@
+/*
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+*/
+
+function grow(x){
+  sum = 1;
+  x.forEach(multiAdd);
+
+  function multiAdd(num) {
+    sum = sum * num;
+  };
+
+  return sum;
+};
+
+/* Best Pratice:
+
+const grow=x=> x.reduce((a,b) => a*b );
+
+*/
