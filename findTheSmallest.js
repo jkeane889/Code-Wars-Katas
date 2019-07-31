89b5538391eec7a5a6b8c377beb01c6d38eab04a
Jonathan Keane <jkeane889@gmail.com>
2019-07-31 18:56:50 -0400

    Updated JS to find smallest integer at smallest index to create smallest integer

--- findTheSmallest.js
+++ findTheSmallest.js
@@ -1,80 +1,108 @@
-/*
-
-You have a positive number n consisting of digits. You can do at most one operation: Choosing the index of a digit in the number, remove this digit at that index and insert it back to another or at the same place in the number in order to find the smallest number you can get.
-
-#Task: Return an array or a tuple or a string depending on the language (see "Sample Tests") with
-
-1) the smallest number you got
-2) the index i of the digit d you took, i as small as possible
-3) the index j (as small as possible) where you insert this digit d to have the smallest number.
-Example:
-
-smallest(261235) --> [126235, 2, 0] or (126235, 2, 0) or "126235, 2, 0"
-126235 is the smallest number gotten by taking 1 at index 2 and putting it at index 0
-
-smallest(209917) --> [29917, 0, 1] or ...
-
-[29917, 1, 0] could be a solution too but index `i` in [29917, 1, 0] is greater than
-index `i` in [29917, 0, 1].
-29917 is the smallest number gotten by taking 2 at index 0 and putting it at index 1 which gave 029917 which is the number 29917.
-
-smallest(1000000) --> [1, 0, 6] or ...
-
-I - large number
-O - array consisting of rearranged number that is the smallest achievable number possible with the given argument, the index of the moved integer, and then the integer that gave us the smallest number.
-C - trimming zeroes?
-E - n/a
-
-*/
-
 function smallest(n) {
-  // convert n to string and then an array to move numbers' positions
-  // create min number
-  // for each of the integers move from the front to end of array, join array, and then retest that integer against the mininum, if it's smaller, set it as the new minimum, do this for every integer in the original number array
-
-  var nArr = n.toString().split('');
-  var sortedNArr = n.toString().split('').sort();
-  var miniNum = n;
-  var takenIndex = 0;
-  var insertIndex = 0;
+  var digits = '0123456789';
+  var digitsArr = digits.split('');
+  var digitsHash = {};
+  var minNum = n;
+  var fromIndex;
+  var toIndex;
   var finalArr = [];
 
-  sortedNArr.forEach(function (value, index) {
-    var currentIndex = index;
-    var newArr = splitAndFilter(n, value);
-    var i = 0;
+  digitsArr.forEach(function(digit) {
+    digitsHash[digit] = [];
+  });
 
-    while (i <= newArr.length) {
-      newArr.splice(i, 0, value);
-      var newNum = parseInt(newArr.join(''));
-      if (newNum < miniNum) {
-        insertIndex = newArr.indexOf(value);
-        takenIndex = nArr.indexOf(value);
-        miniNum = newNum;
-        newArr = splitAndFilter(n, value);
-        i += 1;
-      } else {
-        newArr = splitAndFilter(n, value);
-        i += 1;
+  var nArr = n.toString().split('');
+  nArr.forEach(function(value, index) {
+    for (var key in digitsHash) {
+      if (parseInt(value) === parseInt(key)) {
+        digitsHash[key].push(index);
       };
     };
   });
 
-  finalArr.push(miniNum, takenIndex, insertIndex);
+  // console.log(digitsHash)
 
-  return finalArr;
-};
+  for (var key in digitsHash) {
+    if (digitsHash[key].length !== 0) {
+      var indexArr = digitsHash[key];
+      indexArr.forEach(function (value, index) {
+        var nCopy = n.toString().split('');
+        var nFiltered = nCopy.filter(function (val, idx) {
+          if (value !== idx) {
+            return val;
+          };
+        });
 
-function splitAndFilter(n, val) {
-  var arr = n.toString().split('');
-  var indexOfVal = arr.indexOf(val);
-  var newArr = arr.filter(function (val, index) {
-    if (index !== indexOfVal) {
-      return val;
+        for (var i = 0; i <= nFiltered.length; i++) {
+          nFiltered.splice(i, 0, key);
+          var newNum = parseInt(nFiltered.join(''));
+          if (newNum < minNum) {
+            minNum = newNum;
+            fromIndex = value;
+            toIndex = i;
+            nCopy = n.toString().split('');
+            nFiltered = nCopy.filter(function (val, idx) {
+              if (value !== idx) {
+                return val;
+                };
+            });
+          } else if ((newNum <= minNum) && (value < fromIndex)) {
+            minNum = newNum;
+            fromIndex = value;
+            toIndex = i;
+            nCopy = n.toString().split('');
+            nFiltered = nCopy.filter(function (val, idx) {
+              if (value !== idx) {
+                return val;
+                };
+            });
+          } else {
+            nCopy = n.toString().split('');
+            nFiltered = nCopy.filter(function (val, idx) {
+              if (value !== idx) {
+                return val;
+                };
+            });
+          };
+        };
+      });
     };
-  });
+  };
 
-  return newArr;
+  if (minNum === n) {
+    return [n, 0, 0];
+  } else {
+    finalArr.push(minNum, fromIndex, toIndex);
+    return finalArr;
+  };
 };
 
-console.log(smallest(261235));
+// ASSERT Function Tests
+
+function assertArraysEqual(actual, expected, testName) {
+  var bool = true;
+
+  for (var i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      bool = false;
+    };
+  };
+
+  if ((actual.length === expected.length) && (bool === true)) {
+    console.log(`passed`);
+  } else {
+    console.log(`FAILED [${testName}] Expected '${expected}' but got '${actual}'`);
+  };
+};
+
+assertArraysEqual(smallest(261235), [126235, 2, 0], 'Test arrays equal');
+assertArraysEqual(smallest(209917), [29917, 0, 1], 'Test arrays equal');
+assertArraysEqual(smallest(285365), [238565, 3, 1], 'Test arrays equal');
+assertArraysEqual(smallest(269045), [26945, 3, 0], 'Test arrays equal');
+assertArraysEqual(smallest(296837), [239687, 4, 1], 'Test arrays equal');
+assertArraysEqual(smallest(187863002809), [18786300289, 10, 0], 'Test arrays equal');
+assertArraysEqual(smallest(199819884756), [119989884756, 4, 0], 'Test arrays equal');
+assertArraysEqual(smallest(111111111), [111111111, 0, 0], 'Test arrays equal');
+assertArraysEqual(smallest(3064768930997586), [306476893997586, 9, 0], 'Test arrays equal');
+assertArraysEqual(smallest(982332048103575), [98233204813575, 10, 0], 'Test arrays equal');
+assertArraysEqual(smallest(8476404577913409), [847640457791349, 14, 0], 'Test arrays equal');
